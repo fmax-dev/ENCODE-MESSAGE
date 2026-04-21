86508bb07d7500797f72a4fef43ba34bf0967879
Max Francois <fmax-dev9@users.noreply.github.com>
2026-04-21 00:45:56 -0400

    feat(Menu Display): Introduced a menu display with the following features: - Add New Messages, allowing users to encode new messages - View Encoded Messages, allowing users to view existing encoding messages

--- encode_message.py
+++ encode_message.py
@@ -1,5 +1,31 @@
-# THE SECRET MESSAGE ENCODER
+# Filename to store user encoded messages
+DATA_FILE = "saved_encoded_messages.txt" 
 
+def append_to_file(user_message):
+    """Save an encoded message to the data file."""
+    try:
+        with open(DATA_FILE, 'a') as file:
+            file.write(user_message + "\n")
+        return True  # Indicate success
+    except IOError:  # Catch actual file I/O errors
+        return False  # Indicate failure
+
+# DISPLAY A MENU
+def view_encoded_message(DATA_FILE):
+    """Allow users to view their encoded messages."""
+    with open(DATA_FILE, 'r') as file:
+        lines = file.read().splitlines()
+
+    # print("--- ALL ENCODED MESSAGES: ---")
+    # print(f" {lines}")
+    # print("---------------------")
+
+    print("\n--- All Encoded Messages: ---")
+    for i, line in enumerate(lines, start=1):
+        print(f"    {i}. {line}")
+    print("---------------------")
+
+# 2. COLLECTING user Inputs
 def gathering_user_inputs():
     """Collecting user secret message and shift number."""
     # User secret message
@@ -11,19 +37,14 @@ def gathering_user_inputs():
 
     return user_message, shift_num
 
-def encode_message():
-    # Empty string to store the encode message
-    secret_message = ""
-    
-    print("\n========== WELCOME ==========")
-
-    # 1. DEFINING a string containing all the alphabet letters
+def create_encode_message(user_message, shift_num):
+    """Create the encode message"""
+     # 1. DEFINING a string containing all the alphabet letters
     alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    # 2. COLLECTING user Inputs
-    user_message, shift_num = gathering_user_inputs()
+    # Empty string to store the encode message
+    secret_message = ""
 
-    # Transformation Process
     for char in user_message:
         if char in alphabet:
             current_position = alphabet.index(char)
@@ -34,8 +55,29 @@ def encode_message():
             secret_message += alphabet[new_position]
         else:
             secret_message += char      # Handle non-letters
-    print(f"Here's your secret message: {secret_message}")
+    print(f"\nHere's your secret message: {secret_message}")
+    return secret_message
 
+def encode_message():
+    
+    print("\n========== WELCOME ==========")
+
+    print("\nAll available options:")
+    print("1. Encode a new message")
+    print("2. View encoded messages")
+
+    user_choice = input("\nSelect an option: ").strip()
+
+    # Logic
+    if user_choice == "1":
+        user_message, shift_num = gathering_user_inputs()
+        secret_message = create_encode_message(user_message, shift_num)
+        append_to_file(secret_message)
+
+    elif user_choice == "2":
+        view_encoded_message(DATA_FILE)
+    else:
+        print("\nError: Please select a valid option")
 
 
 if __name__ == "__main__":
